2d7e6e0360edcf59cd6c1d4706f18bf4345cf1e7
claxmo <1610417640@qq.com>
2025-06-09 20:35:58 +0800

    Upload dist files

--- src/components/mainContainer.vue
+++ src/components/mainContainer.vue
@@ -11,12 +11,9 @@
             ref="waterfallRef" 
             :items="store.posts" 
             :width=285 
-            :gap=30 
+            :gap=15 
             :maxCols=5 
             :breakpoints="[
-                { width: 300, itemWidth: 165, gap: 0 },
-                { width: 400, itemWidth: 200, gap: 5 },
-                { width: 500, itemWidth: 250, gap: 10 },
                 { width: 600, itemWidth: 285, gap: 15 },
                 { width: 1200, itemWidth: 285, gap: 30 },
             ]">
